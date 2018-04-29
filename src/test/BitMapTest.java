408cf87ccaeb48dfb521cb26db53ed53641df18c
aqcastan <aqcastan@asu.edu>
2018-04-28 17:06:44 -0700

    attempting bitmaptest

--- src/test/BitMapTest.java
+++ src/test/BitMapTest.java
@@ -4,11 +4,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class BitMapTest {
+import main.com.neet.DiamondHunter.TileMap.Tile;
+import main.com.neet.DiamondHunter.TileMap.TileMap;
 
+public class BitMapTest {
+	TileMap testMngr = new TileMap(16);
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//testMngr.loadTiles("Resources/Tilesets/testtileset.gif");
+		testMngr.loadMap("C:\\Users\\Me\\Documents\\College\\ASU\\SER\\SER 216\\Project\\SER216-DiamondHunter\\Resources\\Maps\\testmap.map");
+		for(int i = 0; i < testMngr.getNumRows(); i++) {
+			if(i == 0 || i == testMngr.getNumRows()-1) {
+				for(int j = 0; j < testMngr.getNumCols(); j++) {
+					testMngr.setPositionImmediately(i, j);
+					System.out.println("("+testMngr.getx()+", "+testMngr.gety()+")");
+					assertTrue(testMngr.getType(i, j) == Tile.BLOCKED);
+				}
+			}
+			else {
+				testMngr.setPositionImmediately(i, 0);
+				System.out.println("("+testMngr.getx()+", "+testMngr.gety()+")");
+				testMngr.setPositionImmediately(i, testMngr.getNumCols()-1);
+				System.out.println("("+testMngr.getx()+", "+testMngr.gety()+")");
+				assertTrue(testMngr.getType(i,0) == Tile.BLOCKED);
+				assertTrue(testMngr.getType(i,testMngr.getNumCols()-1) == Tile.BLOCKED);
+			}
+		}
 	}
 
 }
