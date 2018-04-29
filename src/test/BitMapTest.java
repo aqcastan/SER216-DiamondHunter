81dda46fa4f372e004eb3f94818cc4fd43f3d9f2
aqcastan <aqcastan@asu.edu>
2018-04-29 00:01:05 -0700

    Integration test checking border indices for blocked marker

--- src/test/BitMapTest.java
+++ src/test/BitMapTest.java
@@ -8,23 +8,25 @@ import main.com.neet.DiamondHunter.TileMap.Tile;
 import main.com.neet.DiamondHunter.TileMap.TileMap;
 
 public class BitMapTest {
-	TileMap testMngr = new TileMap(16);
 	
 	@Test
-	public void test() {
-		//testMngr.loadTiles("Resources/Tilesets/testtileset.gif");
-		testMngr.loadMap("C:\\Users\\Me\\Documents\\College\\ASU\\SER\\SER 216\\Project\\SER216-DiamondHunter\\Resources\\Maps\\testmap.map");
+	public void TestBorder() {
+		TileMap testMngr = new TileMap(16);
+		testMngr.loadTiles("/Tilesets/testtileset.gif");
+		//C:\\Users\\Me\\Documents\\College\\ASU\\SER\\SER 216\\Project\\SER216-DiamondHunter\\Resources\\Maps\\testmap.map
+		testMngr.loadMap("/Maps/testmap.map");
 		for(int i = 0; i < testMngr.getNumRows(); i++) {
 			if(i == 0 || i == testMngr.getNumRows()-1) {
 				for(int j = 0; j < testMngr.getNumCols(); j++) {
 					testMngr.setPositionImmediately(i, j);
-					System.out.println("("+testMngr.getx()+", "+testMngr.gety()+")");
+					System.out.print("("+testMngr.getx()+", "+testMngr.gety()+")");
 					assertTrue(testMngr.getType(i, j) == Tile.BLOCKED);
 				}
+				System.out.println();
 			}
 			else {
 				testMngr.setPositionImmediately(i, 0);
-				System.out.println("("+testMngr.getx()+", "+testMngr.gety()+")");
+				System.out.print("("+testMngr.getx()+", "+testMngr.gety()+")");
 				testMngr.setPositionImmediately(i, testMngr.getNumCols()-1);
 				System.out.println("("+testMngr.getx()+", "+testMngr.gety()+")");
 				assertTrue(testMngr.getType(i,0) == Tile.BLOCKED);
@@ -32,5 +34,4 @@ public class BitMapTest {
 			}
 		}
 	}
-
 }
